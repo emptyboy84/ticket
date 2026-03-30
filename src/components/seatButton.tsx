45578800042b761de79dc55114454d2526a8f961
emptyboy84 <emptyboy85@gmail.com>
2026-03-30 12:57:57 +0900

    c

--- src/components/seatButton.tsx
+++ src/components/seatButton.tsx
@@ -5,13 +5,25 @@ import { Seat } from "../types";
 interface SeatButtonProps {
    seat: Seat; // 1. 부모가 주는 좌석 정보 (예매 상태, 등급 등)
    onSeatClick: (targetSeat: Seat) => void; // 2. 클릭 시 부모에게 알려줄 함수(부모가 주는 '리모컨' (함수))
+   isSelected: boolean;//선택 여부
+
 }
 
+
 // SeatButton 컴포넌트
-export default function SeatButton({ seat, onSeatClick }: SeatButtonProps) {
-   // 좌석 상태(status)나 등급(grade)에 따라 배경색을 다르게 하는 로직
+export default function SeatButton({ seat, onSeatClick, isSelected }: SeatButtonProps) {
+   const isPremium = seat.grade === "premium";
+   let seatClass = "w-12 h12 rounded-t-xl rounded-b-md font-bold text-sm transition-all duration-200 flex items-center justify-center border-b-4 ";
    let bgColor = "bg-gray-300";
-   if (seat.status === "booked") bgColor: "bg-gray-100";
+
+   if (seat.status === "booked") {
+      seatClass += "bg-gray-700 border-gray-800 text-gray-600 cursor-not-allowed opacity-50";
+
+   } else if (isSelected) {
+      seatClass += "bg-indigo-500 border-blue-600 text-white";
+   } else if (isPremium) {
+      seatClass += "bg-yellow-500 border-yellow-600 text-white";
+   }
 
 
    return (
